1275844331f1ca28b3fe471d497f9a44aba6c3c3
MohamedShamry <miswer.2019812@iit.ac.lk>
2022-07-02 19:37:37 +0530

    minor fix

--- 1-intro.js
+++ 1-intro.js
@@ -1,8 +1,8 @@
 const amount = 12;
 if(amount < 10){
-    console.log('small number')
+    console.log('small number 1')
 }else{
-    console.log('big number')
+    console.log('big number 2')
 }
 
 console.log(`it's my first node app!!`)
